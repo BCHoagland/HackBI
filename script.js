025eea4d5b15c34f2821ba7909d84edba73fadc0
BCHoagland <bhoggland@gmail.com>
2017-09-25 20:05:36 -0400

    fixed choppy smooth scrolling

--- script.js
+++ script.js
@@ -44,25 +44,14 @@ $(document).ready(function() {
 		updateHideShow();
 	});
 
-	$("a").on('click', function(event) {
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
 
-		// Make sure this.hash has a value before overriding default behavior
-		if (this.hash !== "") {
-			// Prevent default anchor click behavior
-			event.preventDefault();
+	    var target = this.hash;
+	    var $target = $(target);
 
-			// Store hash
-			var hash = this.hash;
-
-			// Using jQuery's animate() method to add smooth page scroll
-			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top - $(hash).height()
-			}, 800, function(){
-
-				// Add hash (#) to URL when done scrolling (default click behavior)
-				window.location.hash = hash;
-			});
-		} // End if
+			$('html, body').stop().animate({
+     'scrollTop': $target.offset().top - $target.height()
+}, 900, 'swing');
 	});
 });
